7df56169ceeaaffee9f31e587618721730e9f36c
graceseo <dbmaster1004@gmail.com>
2019-02-14 08:40:08 -0500

    completed creating beans

---
+++ src/java/club/admin/YCGSDisplayBooksServlet.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package club.admin;
+
+import club.data.*;
+import club.business.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+/**
+ *
+ * @author dbmas
+ */
+public class YCGSDisplayBooksServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            
+            BookIO book= new BookIO();
+             ArrayList<Book> books = book.getBooks(""); 
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
