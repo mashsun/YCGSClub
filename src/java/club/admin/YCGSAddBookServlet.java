61b9c1a562127ab0be3fd44a001b33d839bceedc
graceseo <dbmaster1004@gmail.com>
2019-02-17 09:20:35 -0500

    Added an error jsp page

--- src/java/club/admin/YCGSAddBookServlet.java
+++ src/java/club/admin/YCGSAddBookServlet.java
@@ -15,7 +15,38 @@ import javax.servlet.http.HttpServletResponse;
 public class YCGSAddBookServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+           
+            String url="/YCGSdisplayBooks";
+            String message="";
+            int quantity=0;
+            
+            //get parameters from the request
+            String code=request.getParameter("code");
+            String description=request.getParameter("description");
+            
+            //If the quantity is blank, default it to 0
+            if(request.getParameter("quantity").trim()==null){
+               quantity=0;
+            }else{
+               quantity=Integer.parseInt(  request.getParameter("quantity"));
+            }
+            
+            if(code==null || code==""){
+                message="Book code is required. \n ";
+            }
+            if(description.length()<3){
+                 message=message+"Description must have at least 3 characters.\n";
+            }
+            if(quantity<0){
+                 message=message+"Quantity must be a positive number. \n";
+            }
+            
+            if(message!=null){
+                    url="/YCGSAddBook.jsp";
+            }
+            request.setAttribute("message", message);
+            
+            getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
 
